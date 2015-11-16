ede4486717400710c46013cfba2f33efd2b61d1c
cleestofuh <cleestofuh@CHRISTOPHER-MACBOOK-PRO-YO.local>
2015-11-15 22:28:10 -0800

    got the habits to show up in list.html

--- HW4/js/list.js
+++ HW4/js/list.js
@@ -6,30 +6,51 @@ var query = new Parse.Query(Habit);
 query.find({
   success:function(results) {
     for(var i = 0; i < results.length; i++) {
+      // retrieve habit info
       var object = results[i];
       var name = object.get('habitName');
       var dFreq = object.get('dailyFreq');
       var image = object.get('image');
+      var imagesrc;
 
-      $(function () {
-        // Grab the template script
-        var theTemplateScript = $("#habit-template").html();
+      if(image == "icon1") {
+        imagesrc = "../img/sleep.jpg";
+      } else if (image == "icon2") {
+        imagesrc = "../img/salad.jpg";
+      } else {
+        imagesrc = "../img/run.jpg";
+      }
 
-        // Compile the template
-        var theTemplate = Handlebars.compile(theTemplateScript);
-
-        // Define our data object
-        var context={
-          "habitName": name,
-          "dailyFreq": dFreq,
-        };
-
-        // Pass our data to the template
-        var theCompiledHtml = theTemplate(context);
-
-        // Add the compiled html to the page
-        $('.habit-content').html(theCompiledHtml);
-      });
+      // create elements
+      $("#habit-list").append("<li>\
+                      <ul class='habit-info'>\
+                          <li><div class='habit-name'>"
+                          + name + "</div></li>\
+                          <li><img class='habit-icon' src='" + imagesrc +  "' alt='habit icon'></li>\
+                      </ul>\
+                      <div class='message'>\
+                          <span class='message-total'>\
+                              <strong>" + dFreq + "</strong> days in a row! Best Record: <strong>5</strong><br>\
+                              <svg height='25' width='150'>\
+                                  <line x1='0' y1='0' x2='60' y2='0' style='stroke:rgba(65, 131, 215, 0.8);stroke-width:25' />\
+                                  <line x1='60' y1='0' x2='150' y2='0' style='stroke:rgba(171,171,171,0.6);stroke-width:25' />\
+                              </svg>\
+                          </span><br>\
+                          <span class='message-today'>Completed <strong>1/1</strong> for today!</span>\
+                      </div>\
+                      <div class='habit-op'>\
+                          <button type='button' class='op op-done' onclick='showMsg(this);'' title='done'>\
+                              <img src='../img/done.svg' alt='Done'>\
+                          </button>\
+                          <button type='button' class='op op-edit' onclick='location.href='edit.html'' title='edit habit'>\
+                              <img src='../img/edit.svg' alt='Edit'>\
+                          </button>\
+                          <button type='button' class='op op-del' onclick='deleteHabit(this);'' title='delete habit'>\
+                              <img src='../img/delete.svg' alt='Del'>\
+                          </button>\
+                      </div>\
+                  </li>");
+    }
 
   },
   error:function(error) {
