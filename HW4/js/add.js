a4c05ac01019944f06cb1b335f6474267bfdbba4
Inchul Park <i5park@ucsd.edu>
2015-11-19 03:44:25 -0800

    image add test
    custom icon add test

--- HW4/js/add.js
+++ HW4/js/add.js
@@ -138,3 +138,42 @@
       }
     });
   }
+  
+  function saveJobApp(objParseFile)
+  {
+     var jobApplication = new Parse.Object("JobApplication");
+     jobApplication.set("applicantName", "Joe Smith");
+     jobApplication.set("profileImg", objParseFile);
+     jobApplication.save(null, 
+     {
+        success: function(gameScore) {
+          // Execute any logic that should take place after the object is saved.
+          var photo = gameScore.get("profileImg");
+          $("#profileImg")[0].src = photo.url();
+        },
+        error: function(gameScore, error) {
+          // Execute any logic that should take place if the save fails.
+          // error is a Parse.Error with an error code and description.
+          alert('Failed to create new object, with error code: ' + error.description);
+        }
+     });
+  }
+
+  $('#profilePhotoFileUpload').bind("change", function(e) {
+         var fileUploadControl = $("#profilePhotoFileUpload")[0];
+         var file = fileUploadControl.files[0];
+         var name = file.name; //This does *NOT* need to be a unique name
+         var parseFile = new Parse.File(name, file);
+
+         parseFile.save().then
+         (
+           function() 
+           {
+               saveJobApp(parseFile);
+           }, 
+           function(error) 
+           {
+             alert("error");
+           }
+         );
+  });
